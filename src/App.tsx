e8b3419b4d633dee6e8f95dbf566c065671cf48d
obrienjackson <102945608+obrienjackson@users.noreply.github.com>
2026-02-18 13:42:35 -0500

    analytics

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,13 @@
 import Map from "./components/Map";
 import LiveATC from "./components/LiveATC";
+import { Analytics } from "@vercel/analytics/next"
 
 export default function App() {
   return (
     <>
       <Map />
       <LiveATC />
+      <Analytics />
     </>
   );
 }
